5bf2f33f6e0c0ecf50964e83504c845e569b6d2a
genda <gendalf.mesa@gmail.com>
2023-04-07 23:08:17 +0300

    [v5.0.1 alpha] Added BaseTest.

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,16 +1,16 @@
 package tests;
 
-import org.openqa.selenium.WebDriver;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import testconfig.WebManager;
+import utils.BaseDriver;
 
 import static testconfig.WebManager.getChromeDriver;
 
-public abstract class BaseTest {
-
-    protected WebDriver driver;
+public abstract class BaseTest extends BaseDriver {
 
     @BeforeSuite
     public void setup() {
